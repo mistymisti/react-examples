e3f0c62cf50a15a60a52b30a7e15bcd6c85765b7
MadhuSudhanan Isti <madhusudhanan23@gmail.com>
2022-11-08 17:18:27 +0530

    clearing app.js
    clearing app.js

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -9,25 +9,10 @@ import getVisibleExpenses from './selectors/expenses';
 //import './styles/style.scss';
 
 const store = configureStore();
-
-store.subscribe(() => {
-    console.log(store.getState());
-});
-
-const state = store.getState();
-
-
-//store.dispatch(setTextFilter('bill'));
-store.dispatch(addExpense( { description : 'Gas bill', note : 'Sample Note', amount: 100, createdAt: 2000} ));
-store.dispatch(addExpense( { description : 'Water bill', note : 'Sample Note', amount: 200, createdAt: 1000} ));
-store.dispatch(addExpense( { description : 'Rent', note : 'Sample Note', amount: 109500} ));
-
-
 const jsx = (
     <Provider store={ store }>
         <AppRouter />
     </Provider>
 );
-
 ReactDOM.render(jsx, document.getElementById('app'));
 
