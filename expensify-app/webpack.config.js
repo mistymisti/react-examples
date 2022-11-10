fa6a70fc38afe20bc78cc6bda7cdd6f2772466b8
MadhuSudhanan Isti <madhusudhanan23@gmail.com>
2022-11-10 10:14:21 +0530

    deployment changes
    deployment changes

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,30 +1,30 @@
 const path = require('path');
-
-console.log(__dirname);
-
-module.exports = { 
-    entry: './src/app.js',
-    output: {
-        path : path.join(__dirname, 'public'),
-        filename : 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    return {
+        entry: './src/app.js',
+        output: {
+            path : path.join(__dirname, 'public'),
+            filename : 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
